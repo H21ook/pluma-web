ac7913b980c89e637652ddc8031f79ba1d0eed75
Khishigbayar Unursaikhan <khishigbayar.u@gmail.com>
2025-03-24 22:21:47 +0800

    profile fix

--- js/profile.js
+++ js/profile.js
@@ -1,3 +1,110 @@
+var scrollStep = 100;
+
+function initScroll() {
+  var scroll = document.getElementById("categoryList");
+  var rightButton = document.getElementById("categoryRightButton");
+  var leftButton = document.getElementById("categoryLeftButton");
+
+  if (scroll && rightButton && leftButton) {
+    // scroll end then right hidden button
+    var scrollWidth = scroll?.scrollWidth;
+    var clientWidth = scroll?.clientWidth;
+    var scrollLeft = scroll.scrollLeft;
+
+    var isEnd = scrollWidth - clientWidth === scrollLeft;
+
+    if (scrollWidth > clientWidth) {
+      document
+        .getElementById("categoryRightButton")
+        .classList.remove("invisible", "opacity-0");
+      document
+        .getElementById("categoryRightButton")
+        .classList.add("visible", "opacity-100");
+    }
+
+    if (isEnd) {
+      document
+        .getElementById("categoryRightButton")
+        .classList.remove("visible", "opacity-100");
+      document
+        .getElementById("categoryRightButton")
+        .classList.add("invisible", "opacity-0");
+    }
+
+    // scroll start then left hidden button
+    if (scrollLeft === 0) {
+      document
+        .getElementById("categoryLeftButton")
+        .classList.remove("visible", "opacity-100");
+      document
+        .getElementById("categoryLeftButton")
+        .classList.add("invisible", "opacity-0");
+    }
+  }
+}
+
+function rightScroll() {
+  var scroll = document.getElementById("categoryList");
+  var scrollLeft = scroll.scrollLeft + scrollStep;
+
+  scroll.scrollBy({
+    left: +scrollStep, // Adjust scroll distance
+    behavior: "smooth",
+  });
+
+  // scroll end then right hidden button
+  var scrollWidth = scroll.scrollWidth;
+  var clientWidth = scroll.clientWidth;
+
+  console.log("scrollLeft ", scrollWidth - clientWidth, scrollLeft);
+
+  var isEnd = scrollWidth - clientWidth <= scrollLeft;
+
+  if (isEnd) {
+    document
+      .getElementById("categoryRightButton")
+      .classList.remove("visible", "opacity-100");
+    document
+      .getElementById("categoryRightButton")
+      .classList.add("invisible", "opacity-0");
+  }
+  if (scrollLeft > 0) {
+    document
+      .getElementById("categoryLeftButton")
+      .classList.remove("invisible", "opacity-0");
+    document
+      .getElementById("categoryLeftButton")
+      .classList.add("visible", "opacity-100");
+  }
+}
+
+function leftScroll() {
+  var scroll = document.getElementById("categoryList");
+  const scrollLeft = scroll.scrollLeft - scrollStep;
+
+  scroll.scrollBy({
+    left: -scrollStep, // Adjust scroll distance
+    behavior: "smooth",
+  });
+
+  if (scrollLeft <= 0) {
+    document
+      .getElementById("categoryLeftButton")
+      .classList.remove("visible", "opacity-100");
+    document
+      .getElementById("categoryLeftButton")
+      .classList.add("invisible", "opacity-0");
+  }
+  if (scrollLeft < scrollStep) {
+    document
+      .getElementById("categoryRightButton")
+      .classList.remove("invisible", "opacity-0");
+    document
+      .getElementById("categoryRightButton")
+      .classList.add("visible", "opacity-100");
+  }
+}
+
 function init() {
   initMain();
 
@@ -49,26 +156,29 @@ function init() {
     });
 }
 
-function changeTab(event) {
-  const tabButtons = document.getElementById("tabButtons").children;
-  const tabContents = document.getElementById("tabContents").children;
+window.addEventListener("resize", initScroll);
+window.addEventListener("load", initScroll);
 
-  for (let j = 0; j < tabContents.length; j++) {
-    tabContents[j].classList.add("hidden");
-    if (event.target.dataset.value === tabContents.item(j).dataset.value) {
-      tabContents.item(j).classList.remove("hidden");
-    } else {
-      tabContents.item(j).classList.add("hidden");
+function selectCategory(e) {
+  e.preventDefault();
+  var category = e.target;
+  var allCategoryList = document.getElementById("categoryList").children;
+
+  for (var i = 0; i < allCategoryList.length; i++) {
+    allCategoryList[i].classList.remove("activeCategoryItem");
+    allCategoryList[i].classList.add("inactiveCategoryItem");
+
+    if (category === allCategoryList[i]) {
+      var tabs = document.getElementById("tabs").children;
+      for (var j = 0; j < tabs.length; j++) {
+        if (i === j) {
+          tabs[j].classList.remove("hidden");
+        } else {
+          tabs[j].classList.add("hidden");
+        }
+      }
     }
   }
-
-  for (let i = 0; i < tabButtons.length; i++) {
-    tabButtons.item(i).classList.remove("border-primary");
-    tabButtons
-      .item(i)
-      .classList.add("text-text-secondary", "border-transparent");
-  }
-
-  event.target.classList.remove("text-text-secondary", "border-transparent");
-  event.target.classList.add("border-primary");
+  category.classList.remove("inactiveCategoryItem");
+  category.classList.add("activeCategoryItem");
 }
